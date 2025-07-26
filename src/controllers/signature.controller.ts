7aea6a85b6eca0dbd164cb807fce9b5ecb7fa5bd
thatcelt <fallenincoma@gmail.com>
2025-07-26 07:41:54 +0300

    refactor: deleted unused module; chore: added check to keypair

--- src/controllers/signature.controller.ts
+++ src/controllers/signature.controller.ts
@@ -1,6 +1,6 @@
 import { FastifyReply } from "fastify";
 import { GenerateECDSARequest, GenerateHMACRequest } from "./types";
-import { signECDSA, signHMAC } from "../utils/resolveFrida";
+import { signECDSA, signHMAC, hasAlias, createKeyPair } from "../utils/resolveFrida";
 import { ENCODER } from "../constants";
 
 
@@ -16,6 +16,8 @@ export async function generateHMAC(req: GenerateHMACRequest, reply: FastifyReply
 }
 
 export async function generateECDSA(req: GenerateECDSARequest, reply: FastifyReply) {
+    if (!hasAlias(req.body.userId)) await createKeyPair(req.body.userId);
+    
     try {
         const generatedECDSA = await signECDSA(ENCODER.encode(req.body.payload), req.body.userId);
 
